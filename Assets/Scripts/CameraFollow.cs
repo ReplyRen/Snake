ccdfcfc265745c72ec6d6f8d460542deb0dc3161
unknown <ryphy0420@gmail.com>
2019-10-02 21:00:23 +0800

    第一次提交

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float smooth = 5f;                // how smooth the camera movement is   
+    private Vector3 targetPosition;     // the position the camera is trying to be in  
+    private Transform follow;
+    private void Start()
+    {
+        follow = GameObject.FindWithTag("Player").transform;
+    }
+    private void LateUpdate()
+    {
+        targetPosition = new Vector3(follow.position.x, follow.position.y, -10f); 
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed=5f;
+    private Vector3 moveTo;
+    private void Update()
+    {
+        if ((transform.position - MousePosition()).magnitude > 1f) 
+            moveTo = MoveDirection(transform.position, MousePosition());
+        Move(moveTo);
+    }
+    private void Move(Vector3 targetDir)
+    {
+        gameObject.transform.Translate(targetDir * moveSpeed * Time.deltaTime);
+        
+    }
+    private Vector3 MousePosition()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+        Vector3 v1 = Camera.main.ScreenToWorldPoint(mousePos);
+        v1 = new Vector3(v1.x, v1.y, 0f);
+        return v1;
+    }
+    private Vector3 MoveDirection(Vector3 from,Vector3 to)
+    {
+        Vector3 vector = to - from;
+        vector = vector.normalized;
+        return vector;
+    }
+}
